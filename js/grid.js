6a34195c75673eb3ccc1096ea0b4264ae975aff1
Robert Thumpston <thumpston@gmail.com>
2013-08-06 16:32:57 +0100

    Build player object

--- js/grid.js
+++ js/grid.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
 	var height = 20;
 	var width = 100;
 	var grid = new Array(width);
-	
+		
 	for (var i=0; i<width; i++) {
 		grid[i] = new Array(height);
 	}
+	
+	var player1 = new Player(4, 5, 'X');
 		
 	populateGrid();
-	grid[1][2] = player1;
 	printGrid();
 	
 	
@@ -30,6 +31,12 @@ $(document).ready(function() {
 			}
 			$('body').append('<br>');
 		}
+	}
+	
+	function Player(xPos, yPos, character) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.character = character;
 	}
 	
 });
