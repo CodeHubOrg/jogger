abcd779b7f0143f6a0a8357e2b797969b06b613f
Robert Thumpston <thumpston@gmail.com>
2013-08-06 22:35:30 +0100

    We have movement

--- js/grid.js
+++ js/grid.js
@@ -1,18 +1,84 @@
 $(document).ready(function() {
 	
-	var height = 20;
-	var width = 100;
+	var height = 10;
+	var width = 10;
 	var grid = new Array(width);
 		
 	for (var i=0; i<width; i++) {
 		grid[i] = new Array(height);
 	}
 	
-	var player1 = new Player(0, 0, 'X');
-		
+	var player1 = new Player(5, 5, 'X');
+	
+	//Set up initial environment	
 	populateGrid();
 	updateGrid(player1);
 	printGrid();
+	
+	//Listen for key press
+	$('body').keydown(function(e) {
+		switch(e.keyCode) {
+			case 37: //left arrow
+				updatePlayer("left");
+				break;
+			case 38: //up arrow
+				updatePlayer("up");
+				break;
+			case 39: //right arrow
+				updatePlayer("right");
+				break;
+			case 40: //down arrow
+				updatePlayer("down");
+				break;
+			default:
+				//Do nothing
+		}
+
+		updateGrid(player1);
+		printGrid();
+		
+	});
+	
+	
+	
+	function updatePlayer(direction) {
+		switch(direction) {
+			case "left":
+				if (player1.xPos > 0) {
+					player1.xPos--;
+				}
+				else {
+					alert("EDGE");
+				}
+				break;
+			case "up":
+				if (player1.yPos > 0) {
+					player1.yPos--;
+				}
+				else {
+					alert("EDGE");
+				}
+				break;
+			case "right":
+				if (player1.xPos < width-1) {
+					player1.xPos++;
+				}
+				else {
+					alert("EDGE");
+				}
+				break;
+			case "down":
+				if (player1.yPos < height-1) {
+					player1.yPos++;
+				}
+				else {
+					alert("EDGE");
+				}
+				break;
+			default:
+				//De nada
+		}
+	}
 	
 	
 	//TODO At some point pass a grid as an argument, rather than using global
@@ -26,6 +92,7 @@ $(document).ready(function() {
 	
 	//TODO accept an array of players as argument
 	function updateGrid(player) {
+		populateGrid();
 		grid[player.xPos][player.yPos] = player.character;
 	}
 	
