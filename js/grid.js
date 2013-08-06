3e390864b9c135c2feed92b6fd9e299ec9038801
Robert Thumpston <thumpston@gmail.com>
2013-08-06 16:29:59 +0100

    Rewrites with methods

--- js/grid.js
+++ js/grid.js
@@ -2,30 +2,34 @@ $(document).ready(function() {
 	
 	var height = 20;
 	var width = 100;
-	var player1 = '1';
 	var grid = new Array(width);
 	
-
-	//Build 2D array
 	for (var i=0; i<width; i++) {
 		grid[i] = new Array(height);
 	}
-
-	//Fill with zeroes
-	for (var i=0; i<width; i++) {
-		for (var j=0; j<height; j++) {
-			grid[i][j] = '0';
+		
+	populateGrid();
+	grid[1][2] = player1;
+	printGrid();
+	
+	
+	//At some point pass a grid as an argument, rather than using global
+	function populateGrid() {
+		for (var i=0; i<width; i++) {
+			for (var j=0; j<height; j++) {
+				grid[i][j] = '0';
+			}
 		}
 	}
-
-	grid[3][2] = player1;
-
-	//Output to screen
-	for (var j=0; j<height; j++) {
-		for (var i=0; i<width; i++) {
-			$('#grid').append(grid[i][j]);
+	
+	function printGrid() {
+		$('body').empty();
+		for (var j=0; j<height; j++) {
+			for (var i=0; i<width; i++) {
+				$('body').append(grid[i][j]);
+			}
+			$('body').append('<br>');
 		}
-		$('#grid').append('<br>');
 	}
 	
 });
