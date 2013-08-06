fcceaa1fad0d4704c41bfd3d9a938d0e032c1a02
Robert Thumpston <thumpston@gmail.com>
2013-08-06 13:12:56 +0100

    Write zeroes to screen

--- js/grid.js
+++ js/grid.js
@@ -12,11 +12,13 @@ for (var i=0; i<width; i++) {
 for (var i=0; i<width; i++) {
 	for (var j=0; j<height; j++) {
 		x[i][j] = '0';
-	}
+	}	
 }
 
-/*for (var i=0; i<width; i++) {
-	for (var j=0; ;<height; j++) {
+//Output to screen
+for (var j=0; j<height; j++) {
+	for (var i=0; i<width; i++) {
 		document.write(x[i][j]);
 	}
-}*/
+	document.write('<br>');
+}
