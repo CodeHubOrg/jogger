333c0e02acd96c5bb3a250234bb2dc46abb74e49
Robert Thumpston <thumpston@gmail.com>
2013-08-06 15:59:25 +0100

    Rewrite with jQuery.

--- js/grid.js
+++ js/grid.js
@@ -1,24 +1,31 @@
-var height = 20;
-var width = 30;
-var x = new Array(width);
+$(document).ready(function() {
+	
+	var height = 20;
+	var width = 100;
+	var player1 = '1';
+	var grid = new Array(width);
+	
 
-//Build 2D array
-for (var i=0; i<width; i++) {
-	x[i] = new Array(height);
-}
-
-
-//Fill with zeroes
-for (var i=0; i<width; i++) {
-	for (var j=0; j<height; j++) {
-		x[i][j] = '0';
-	}	
-}
-
-//Output to screen
-for (var j=0; j<height; j++) {
+	//Build 2D array
 	for (var i=0; i<width; i++) {
-		document.write(x[i][j]);
+		grid[i] = new Array(height);
 	}
-	document.write('<br>');
-}
+
+	//Fill with zeroes
+	for (var i=0; i<width; i++) {
+		for (var j=0; j<height; j++) {
+			grid[i][j] = '0';
+		}
+	}
+
+	grid[3][2] = player1;
+
+	//Output to screen
+	for (var j=0; j<height; j++) {
+		for (var i=0; i<width; i++) {
+			$('#grid').append(grid[i][j]);
+		}
+		$('#grid').append('<br>');
+	}
+	
+});
